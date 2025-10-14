9138e502c66599d3c062c4a4cf2794d7f45f992e
dr01d3r <droider007@gmail.com>
2025-10-14 09:49:40 -0500

    fixed path issue

--- ExternalModule.php
+++ ExternalModule.php
@@ -6,12 +6,12 @@ use ExternalModules\ExternalModules;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-require_once 'traits\APIUtils.php';
-require_once 'traits\CronUtils.php';
-require_once 'traits\ModuleUtils.php';
-require_once 'traits\REDCapUtils.php';
-require_once 'traits\RequestHandlers.php';
-require_once 'traits\GooglePhotosUtils.php';
+require_once 'traits/APIUtils.php';
+require_once 'traits/CronUtils.php';
+require_once 'traits/ModuleUtils.php';
+require_once 'traits/REDCapUtils.php';
+require_once 'traits/RequestHandlers.php';
+require_once 'traits/GooglePhotosUtils.php';
 
 class ExternalModule extends AbstractExternalModule {
     use APIUtils;
@@ -20,6 +20,4 @@ class ExternalModule extends AbstractExternalModule {
     use REDCapUtils;
     use RequestHandlers;
     use GooglePhotosUtils;
-
-    const ALBUM_TITLE = "REDCapCon 2025 Photobooth";
 }
